61f2aa9c2ca56de0c4a5fd798d660de945934ab7
lihangyu1913092 <2415358701@qq.com>
2022-05-17 19:53:23 +0800

    add element depend css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import router from './router'
+import 'element-plus/dist/index.css'
 
 
 const app = createApp(App)
