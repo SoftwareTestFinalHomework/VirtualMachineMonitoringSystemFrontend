1daf11681b364c8b768daa69f025e47a2cb68b04
lihangyu1913092 <2415358701@qq.com>
2022-05-19 21:25:22 +0800

    server function

--- src/components/CreateServerPage.vue
+++ src/components/CreateServerPage.vue
@@ -1,16 +1,67 @@
 <template>
-
+    <el-row>
+        <el-col :span="6"></el-col>
+        <el-col :span="12">
+            <el-input v-model="serverIP" placeholder="You must use IPv4 address" clearable>
+                <template #prepend>Please Input Server's IP</template>
+            </el-input>
+        </el-col>
+        <el-col :span="6"></el-col>
+    </el-row>
+    <el-row>
+        <el-col :span="6"></el-col>
+        <el-col :span="12">
+            <el-input maxlength="25"
+                      v-model="serverName"
+                      placeholder="Server's name can not longer than 25 characters"
+                      clearable
+                      show-word-limit>
+                <template #prepend>Please Input Server's Name</template>
+            </el-input>
+        </el-col>
+    </el-row>
+    <el-row>
+        <el-col :span="6"></el-col>
+        <el-col :span="12">
+            <el-button type="primary">
+                Check Connection
+            </el-button>
+            <el-button type="primary">
+                Create Server
+            </el-button>
+        </el-col>
+        <el-col :span="6"></el-col>
+    </el-row>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
     name: "CreateServerPage",
     setup(props,context){
+        const serverIP = ref('')
+        const serverName = ref('')
 
+        return {
+            serverIP,
+            serverName
+        }
     }
 }
 </script>
 
 <style scoped>
-
+.el-row{
+    font-size: 18px;
+    margin-top: 50px;
+}
+.el-row:first-child{
+    margin-top: 150px;
+}
+.el-input{
+    font-size: 18px;
+}
+.el-col{
+    text-align: center;
+}
 </style>
--- src/components/DestroyServerPage.vue
+++ src/components/DestroyServerPage.vue
@@ -1,13 +1,51 @@
 <template>
-
+    <el-row>
+        <el-col :span="6"></el-col>
+        <el-col :span="12">
+            <el-input v-model="serverIP" placeholder="You must use IPv4 address" clearable>
+                <template #prepend>Please Input Server's IP</template>
+            </el-input>
+        </el-col>
+        <el-col :span="6"></el-col>
+    </el-row>
+    <el-row>
+        <el-col :span="6"></el-col>
+        <el-col :span="12">
+            <el-button type="danger">
+                Delete the Server
+            </el-button>
+        </el-col>
+        <el-col :span="6"></el-col>
+    </el-row>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
-    name: "DestroyServerPage"
+    name: "DestroyServerPage",
+    setup(props,context){
+        const serverIP = ref('')
+
+        return {
+            serverIP
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+.el-row{
+    font-size: 18px;
+    margin-top: 50px;
+}
+.el-row:first-child{
+    margin-top: 150px;
+}
+.el-input{
+    font-size: 18px;
+}
+.el-col{
+    text-align: center;
+}
 </style>
