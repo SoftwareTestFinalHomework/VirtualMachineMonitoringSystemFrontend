d94af8357723425b1d89febda1bc9c727d656485
lihangyu1913092 <2415358701@qq.com>
2022-05-20 13:31:14 +0800

    build destroy virtual Machine page

--- src/components/DestroyVirtualMachinePage.vue
+++ src/components/DestroyVirtualMachinePage.vue
@@ -1,13 +1,49 @@
 <template>
-
+    <el-row>
+        <el-col :span="1"></el-col>
+        <el-col :span="22">
+            <el-card>
+                <el-row>
+                    <el-col :span="6"></el-col>
+                    <el-col :span="4">
+                        <span>Machine Name:</span>
+                    </el-col>
+                    <el-col :span="8">
+                        <el-input v-model="virtualMachineName" class="input-machine-name" placeholder="Please input Virtual Machine Name" clearable></el-input>
+                    </el-col>
+                    <el-col :span="6"></el-col>
+                </el-row>
+                <el-row>
+                    <el-col class="destroy-button">
+                        <el-button type="danger">Destroy the Virtual Machine</el-button>
+                    </el-col>
+                </el-row>
+            </el-card>
+        </el-col>
+        <el-col :span="1"></el-col>
+    </el-row>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
-    name: "DestroyVirtualMachinePage"
+    name: "DestroyVirtualMachinePage",
+    setup(props,context){
+        const virtualMachineName = ref('')
+
+        return {
+            virtualMachineName
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+.el-row{
+    margin-top: 20px;
+}
+.destroy-button{
+    text-align: center;
+}
 </style>
